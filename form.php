bd0efb9d6dfd1a11bdc6d54c73648af08bb7883b
rkw63926 <rkw63926@educastur.es>
2023-03-20 13:37:39 +0100

    v2.1

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="imagenes/favicon.png">
+    <link rel="stylesheet" href="estilos/estiloGijon.css">
+    <title>Formulario Contacto | Visita guiada</title>
+</head>
+<body>
+    <?php
+    $nombre = $_REQUEST["nombre"];
+    
+    ?>
+</body>
+</html>
